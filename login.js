756f1c27232c45b3f790114d088f888c7732873f
Omid Graf <omidgraf@Omids-MacBook-Pro.local>
2022-03-20 15:39:59 -0700

    JS for login, nav prework

---
+++ login.js
@@ -0,0 +1,27 @@
+// Get the modal
+var modal = document.getElementById('id01');
+var modal2 = document.getElementById('id02');
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+    else if (event.target == modal2) {
+      modal2.style.display = "none";
+    }
+}
+
+// Validate Password:
+var password = document.getElementById("psw"), confirm_password = document.getElementById("confirm-psw");
+
+function validatePassword(){
+  if(password.value != confirm_password.value) {
+    confirm_password.setCustomValidity("Passwords Don't Match");
+  } else {
+    confirm_password.setCustomValidity('');
+  }
+}
+
+password.onchange = validatePassword;
+confirm_password.onkeyup = validatePassword;
